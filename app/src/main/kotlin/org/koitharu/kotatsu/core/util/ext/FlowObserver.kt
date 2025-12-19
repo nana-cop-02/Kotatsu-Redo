4c22ca614cc4d5aed7564afb49bd2bc4bf6cad8c
nana-cop-02 <theswapnil.me@gmail.com>
2025-12-19 19:56:32 +0000

    redo

--- app/src/main/kotlin/org/koitharu/kotatsu/core/util/ext/FlowObserver.kt
+++ app/src/main/kotlin/org/koitharu/kotatsu/core/util/ext/FlowObserver.kt
@@ -62,25 +62,19 @@ inline fun <T> Flow<Event<T>?>.observeEvent(owner: LifecycleOwner, minState: Lif
 	})
 }
 
-inline fun <T> Flow<T>.observe(owner: LifecycleOwner, crossinline action: (T) -> Unit) {
-	observe(owner, object : FlowCollector<T> {
-		override suspend fun emit(value: T) = action(value)
-	})
+// Non-suspend overloads for compatibility with existing code
+inline fun <T> Flow<T>.observe(owner: LifecycleOwner, noinline action: (T) -> Unit) {
+	observe(owner) { action(it) }
 }
 
-inline fun <T> Flow<T>.observe(owner: LifecycleOwner, minState: Lifecycle.State, crossinline action: (T) -> Unit) {
-	observe(owner, minState, object : FlowCollector<T> {
-		override suspend fun emit(value: T) = action(value)
-	})
+inline fun <T> Flow<T>.observe(owner: LifecycleOwner, minState: Lifecycle.State, noinline action: (T) -> Unit) {
+	observe(owner, minState) { action(it) }
 }
 
-inline fun <T> Flow<Event<T>?>.observeEvent(owner: LifecycleOwner, crossinline action: (T) -> Unit) {
-	observeEvent(owner, Lifecycle.State.STARTED, action)
+inline fun <T> Flow<Event<T>?>.observeEvent(owner: LifecycleOwner, noinline action: (T) -> Unit) {
+	observeEvent(owner) { action(it) }
 }
 
-inline fun <T> Flow<Event<T>?>.observeEvent(owner: LifecycleOwner, minState: Lifecycle.State, crossinline action: (T) -> Unit) {
-	observeEvent(owner, minState, object : FlowCollector<T> {
-		override suspend fun emit(value: T) = action(value)
-	})
+inline fun <T> Flow<Event<T>?>.observeEvent(owner: LifecycleOwner, minState: Lifecycle.State, noinline action: (T) -> Unit) {
+	observeEvent(owner, minState) { action(it) }
 }
-
